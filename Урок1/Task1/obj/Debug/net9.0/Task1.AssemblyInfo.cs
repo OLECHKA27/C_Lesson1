f347ada6663e085bd088986df86aacedee05ef73
RUTT <rutt@inbox.ru>
2025-02-16 21:16:46 +0300

    Повторное коментирование Task1 тренировка

--- Урок1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Урок1/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db63ba03d1bccfa20a2d0b4c7c62d9e159beb1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
