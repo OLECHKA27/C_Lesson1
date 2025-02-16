1ccc05ec8deab94eb700494a35cfa8b1d1c6fff8
RUTT <rutt@inbox.ru>
2025-02-16 22:17:20 +0300

    Добавила все типы переменных

--- Урок1/Task1/Program.cs
+++ Урок1/Task1/Program.cs
@@ -1,3 +1,11 @@
-﻿Console.WriteLine("Hello world");
+﻿//Console.WriteLine("Hello world");
 
+
+int value = 100;
+Console.WriteLine(value);
+
+double D = 5,55;
+char C = '!';
+bool b = true;
+Console.WriteLine(D+C+b);
 
