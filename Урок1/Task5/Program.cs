bbe3ae1587fb2e828fc03e8bfac21772226696f6
RUTT <rutt@inbox.ru>
2025-02-17 01:34:49 +0300

    Цикл Нат.числа Task5

---
+++ Урок1/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿int N =9;
+for (int A =0; A<=N; A++)
+{Console.WriteLine($"Ряд натуральных чисел от 1 до {N}:+{A+1}");}
+
+
+int L = 9;
+string result = string.Empty;
+
+for (int i = 1; i <= L; i++)
+{
+    if (i != 1)
+        result += ", ";
+    
+    result += i.ToString();
+}
+
+Console.WriteLine(result);
