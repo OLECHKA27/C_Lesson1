9a3cf2071f5ac7d5d230d4d52137641c058b00e2
RUTT <rutt@inbox.ru>
2025-02-17 00:58:07 +0300

    Решение задачи с гирями №4

---
+++ Урок1/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a1 = 120;
+int a2 = 8;
+int a3 = 56;
+int a4 = 55;
+int a5 = 4;
+int Max = a1;
+
+if (a2 > Max)
+{Max =a2;
+}
+if (a3 > Max)
+{Max = a3;
+}
+if ( a4 > Max)
+{Max = a4;
+}
+if (a5 > Max)
+{
+Max = a5;
+}
+Console.WriteLine($"Максисальный вес Max = {Max}");
